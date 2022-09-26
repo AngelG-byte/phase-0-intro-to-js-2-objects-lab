974982f0a4fa5f7b1546bd5936c68f654ca05cf9
Angel Marte <robotking134@gmail.com>
2022-09-26 19:51:41 -0400

    commit tree 7304540d31c375210c2103b8eccd64dec734362c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = 
+{  name: "angel",
+streetAddress: "11 Broadway",
+    }
+function updateEmployeeWithKeyAndValue(object, key, value){
+    return { ...employee,["name"]: "Sam", ["streetAddress"]:"11 Broadway", };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value){
+    employee["name"]= "Sam";
+    employee["streetAddress"]= "12 Broadway"
+    return employee;
+}
+function deleteFromEmployeeByKey(object,key){
+return {...employee[delete "name"]}
+}
+function destructivelyDeleteFromEmployeeByKey(object,key){
+    delete employee.name
+    return employee;
+}
